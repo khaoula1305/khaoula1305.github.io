23b6387290dcdd8895d51c8fad84d3083398167e
Benchari khaoula <khaoulabenchari9@gmail.com>
2020-12-02 00:29:06 +0100

    more languages

---
+++ src/utils/imagesDic.js
@@ -0,0 +1,48 @@
+// technologies images
+import react from '../img/language/react.png';
+import react_native from '../img/language/react-native.png';
+import redux from '../img/language/redux.svg';
+import javascript from '../img/language/javascript.svg';
+import html_and_css from '../img/language/html_and_css.svg';
+import node from '../img/language/node.png';
+import git from '../img/language/git.svg';
+import python from '../img/language/python.svg';
+import mongoDB from '../img/language/mongodb.svg';
+import mysql from '../img/language/mysql.svg';
+import php from '../img/language/php.svg';
+import express from '../img/language/expressjs.svg';
+import firebase from '../img/language/firebase.svg';
+import java from '../img/language/java.png'
+// projects images
+
+
+const lngLogo = {
+    react,
+    javascript,
+    html_and_css,
+    node,
+    git,
+    python,
+    react_native,
+    mongoDB,
+    mysql,
+    php,
+    express,
+    firebase,
+    redux,
+    java,
+}
+/*
+const projectsImgs = {
+    img2save,
+    masplay,
+    guarico,
+    nestedSelects,
+    nestedSelectsReact,
+    chat
+}*/
+
+export {
+    lngLogo,
+   // projectsImgs
+}
