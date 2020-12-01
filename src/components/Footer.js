b28f0a444a625bf296384ae3c971d27ae6c21ef3
Benchari khaoula <khaoulabenchari9@gmail.com>
2020-12-01 22:50:27 +0100

    images

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Social from './Social';
+
+class Footer extends Component {
+    render() { 
+        return ( <footer class="footer">
+            <p class="footer__title">khaoula1305</p>
+            <div class="footer__social">
+                <Social/>
+            </div>
+            <p>&#169; 2020 copyright all right reserved</p>
+        </footer>
+
+ );
+    }
+}
+ 
+export default Footer;
